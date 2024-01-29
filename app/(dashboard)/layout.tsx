847bbe70263e080f395e44a7d6ed649db0dcc919
Angie Rojas <nataliart4@gmail.com>
2024-01-29 14:28:16 -0500

    Reviewing main layout and define a layout for the dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+const DashboardLayout = ({children}) => {
+  return <div>
+    <h1 className="font-bold">Dashboard section</h1>
+    <div>{children}</div>
+  </div>
+}
+
+export default DashboardLayout;
